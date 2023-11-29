4eb28b61973602d639f43e34cf4e9c259697a4d5
meedouazane <mohamedouazane@yahoo.com>
2023-11-29 13:33:49 -0800

    16-binary_tree_is_perfect.c

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: is a pointer to the root node of the tree to check
+ * Return: 1 if tree is full otherwise 0.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	const binary_tree_t *current;
+
+	if (tree == NULL)
+		return (0);
+	current = tree;
+	if (current->right == NULL && current->left == NULL)
+		return (1);
+	if (current->right != NULL && current->left != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree.
+ * @tree: is a pointer to the root node of the tree.
+ * Return: Number of the nodes with at least 1 child.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+		nodes++;
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes);
+}
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: is a pointer to the root node of the tree to check
+ * Return: 1 if tree is perferct otherwise 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_nodes(tree->left) % 2 != 0 &&
+			binary_tree_nodes(tree->right) % 2 != 0)
+		return (binary_tree_is_full(tree->left) * binary_tree_is_full(tree->right));
+	return (0);
+}
