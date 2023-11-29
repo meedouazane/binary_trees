4cf420ef7f5e3b105d5ffa169390f2d1527e4514
meedouazane <mohamedouazane@yahoo.com>
2023-11-29 05:44:26 -0800

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,27 +1,47 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_height_left - function that measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: height of left.
+ */
+size_t binary_tree_height_left(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+
+	if (tree == NULL)
+		return (0);
+	left_height++;
+	left_height += binary_tree_height_left(tree->left);
+	return (left_height);
+}
+/**
+ * binary_tree_height_right- function that measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: height of right.
+ */
+size_t binary_tree_height_right(const binary_tree_t *tree)
+{
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+	right_height++;
+	right_height += binary_tree_height_right(tree->right);
+	return (right_height);
+}
 /**
  * binary_tree_height - function that measures the height of a binary tree
  * @tree: is a pointer to the root node of the tree to measure the height.
  * Return: height of tree.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
-
 	if (tree == NULL)
 		return (0);
-	while (tree->left != NULL || tree->right != NULL)
-	{
-		if (tree->left != NULL)
-		{
-			height++;
-			tree = tree->left;
-		}
-		else if (tree->right != NULL)
-		{
-			height++;
-			tree = tree->right;
-		}
-	}
-	return (height);
+	if (binary_tree_height_right(tree) <= binary_tree_height_left(tree))
+		return (binary_tree_height_left(tree) - 1);
+	else
+		return (binary_tree_height_right(tree) - 1);
 }
